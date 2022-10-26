f7b753459aa2b110251dfd884f9a84d7c6169d7b
lincongyi <88583354@163.com>
2022-10-26 17:14:04 +0800

    completed

--- src/index.js
+++ src/index.js
@@ -7,8 +7,9 @@ import {
 } from "react-router-dom";
 import Root, { loader as rootLoader, action as rootAction, } from "./routes/root";
 import ErrorPage from './error-page';
-import Contact, { loader as contactLoader, } from "./contact";
+import Contact, { loader as contactLoader, action as contactAction, } from "./contact";
 import EditContact, { action as editAction, } from "./routes/edit";
+import Index from "./routes/index";
 import { action as destroyAction } from "./routes/destroy";
 
 const router = createBrowserRouter([
@@ -18,23 +19,30 @@ const router = createBrowserRouter([
     errorElement: <ErrorPage />,
     loader: rootLoader,
     action: rootAction,
-    children: [    
+    children: [
       {
-        path: "contacts/:contactId",
-        element: <Contact />,
-        loader: contactLoader,
-      },
-      {
-        path: "contacts/:contactId/edit",
-        element: <EditContact />,
-        loader: contactLoader,
-        action: editAction,
-      },
-      {
-        path: "contacts/:contactId/destroy",
         errorElement: <ErrorPage />,
-        action: destroyAction,
-      },
+        children: [
+          {index: true, element: <Index />},
+          {
+            path: "contacts/:contactId",
+            element: <Contact />,
+            loader: contactLoader,
+            action: contactAction,
+          },
+          {
+            path: "contacts/:contactId/edit",
+            element: <EditContact />,
+            loader: contactLoader,
+            action: editAction,
+          },
+          {
+            path: "contacts/:contactId/destroy",
+            errorElement: <ErrorPage />,
+            action: destroyAction,
+          },
+        ]
+      }
     ]
   },
 ]);
